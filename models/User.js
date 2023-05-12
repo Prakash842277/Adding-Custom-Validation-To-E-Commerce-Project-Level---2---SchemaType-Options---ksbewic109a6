497d15ef90affe982ff88c07712c15da643be7d8
Prakash Chandra <prakash140chandra@gmail.com>
2023-05-12 12:59:08 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,37 @@
-/* User Model */
+// /* User Model */
+// const mongoose = require('mongoose');
+
+// /*
+// Add custom validation to the User model
+// Validate that the username is at least 4 characters long, doesn't contain spaces, and doesn't contain special characters
+// Also add a validation for checking password length. Password should be atleast 8 chaaacters long.
+// Throw an error is the Validation fails.
+// */
+// const userSchema = new mongoose.Schema(
+//     {
+//         username: {
+//             type: String,
+//             required: true,
+//             unique: true,
+//             //Add a Custom Validation Here
+//         },
+//         email: {
+//             type: String,
+//             required: true,
+//             unique: true
+//         },
+//         password: {
+//             type: String,
+//             required: true,
+//             //Add a Custom Validation Here
+//         }
+//     },
+//     { timestamps: true }
+// );
+
+// module.exports = mongoose.model('User', userSchema);
+
+
 const mongoose = require('mongoose');
 
 /*
@@ -13,7 +46,12 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
             unique: true,
-            //Add a Custom Validation Here
+            validate: {
+                validator: function(v) {
+                    return /^[^\s\W]{4,}$/.test(v);
+                },
+                message: props => `${props.value} is not a valid username! Username should be at least 4 characters long, without spaces or special characters!`
+            }
         },
         email: {
             type: String,
@@ -23,11 +61,15 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true,
-            //Add a Custom Validation Here
+            validate: {
+                validator: function(v) {
+                    return /^.{8,}$/.test(v);
+                },
+                message: props => `${props.value} is not a valid password! Password should be at least 8 characters long!`
+            }
         }
     },
     { timestamps: true }
 );
 
 module.exports = mongoose.model('User', userSchema);
-
